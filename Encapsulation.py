5b8438f4b388d57dee5c294a3595a94ad78afdd7
Kartik Sharma <69217796+kartik2828@users.noreply.github.com>
2024-01-15 23:08:16 +0530

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,17 +1,31 @@
-# How to use getter and setter meathod in Python
+# How to use getter and setter method in Python
 
 class student:
     def __init__(self):
         self.__name = ""
 
-    def getname(self):    #Getter meathod (basically used to get the value)
+    def getname(self):    #Getter method (basically used to get the value)
         return self.__name
 
-    def setname(self,name):  #Setter meathod  (basically used to set the values)
+    def setname(self,name):  #Setter method  (basically used to set the values)
         self.__name=name
 
 obj = student()
 obj.setname("Kartik")
 name = obj.getname()
 print(name)
+
+--------------------------------------------------------------------------------------------------
+# Program 1
+
+class student:
+    __name = "Kartik"
+    def __init__(self):
+        print(self.__name)
+        self.__displayinfo()
+
+    def __displayinfo(self):
+        print("Hello Kartik")
+
+obj = student()
 
